5c7b9e193dfcdb6fa77d71a5548e76c691584bdc
imran <imrnbdshiscool@yahoo.co.in>
2018-02-18 21:06:13 +0100

    watcher makes project by monitoring changes
     taking only one arg == time interval (in sec) to monitor

---
+++ makeWatcher.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Watch for changes: https://stackoverflow.com/a/20436658
+
+args=("$@")
+
+# Check if the first argument is a number (watch time interval), else use the default 10 seconds
+if (($# == 1))
+then
+    watchEvery=${args[1]}
+else
+    watchEvery=10
+fi
+
+# Monitor changes or file creations based on time interval
+inotifywait -e create,modify --exclude '.*\.swp|.*\.git' -rmq . |
+while read
+do
+    make
+    sleep $watchEvery
+done
